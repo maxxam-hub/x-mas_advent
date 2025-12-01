ec821ca61ea1710837a1ff3f47511dbe249ca1d7
unknown <max.xit234@gmail.com>
2025-12-01 23:12:09 +0300

    part one answer 1081

--- day_1/Program.cs
+++ day_1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics.SymbolStore;
+
+class Program
+{
+    static void Main()
+    {
+        int k = 0;
+        int current = 50;
+        string rotation = "";
+
+        while (isEnd(ref rotation))
+        {
+            if (rotation[0] == 'R')
+            {
+                current = (current + int.Parse(rotation.Substring(1))) % 100;
+                k = current == 0 ? k + 1 : k;
+            }
+            if (rotation[0] == 'L')
+            {
+                current = (current - int.Parse(rotation.Substring(1)) + 100) % 100;
+                k = current == 0 ? k + 1 : k;
+            }
+        }
+
+        Console.WriteLine(k);
+    }
+
+    static bool isEnd(ref string str)
+    {
+        str = Console.ReadLine();
+
+        if (str == "" || str == "\n" || str == " ") return false;
+        else return true;
+    }
+}
