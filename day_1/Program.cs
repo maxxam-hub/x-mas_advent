39c6f303214bc9ce6909c68902880c5d39e37799
unknown <max.xit234@gmail.com>
2025-12-02 00:12:40 +0300

    in progress

--- day_1/Program.cs
+++ day_1/Program.cs
@@ -5,25 +5,28 @@ class Program
 {
     static void Main()
     {
-        int k = 0;
-        int current = 50;
+        int k = 0, curK = 0, temp = 0, current = 50;
         string rotation = "";
 
         while (isEnd(ref rotation))
         {
+            temp = int.Parse(rotation.Substring(1));
+
             if (rotation[0] == 'R')
             {
-                current = (current + int.Parse(rotation.Substring(1))) % 100;
-                k = current == 0 ? k + 1 : k;
+                curK = (temp + current) / 100;
+                current = (current + temp) % 100;
             }
             if (rotation[0] == 'L')
             {
-                current = (current - int.Parse(rotation.Substring(1)) + 100) % 100;
-                k = current == 0 ? k + 1 : k;
+                curK = current > temp ? 0 : (current - temp - 100) / -100;
+                current = curK == 0 ? (current - temp + 100) % 100 : (current - temp + 100 * curK) % 100;
             }
-        }
 
-        Console.WriteLine(k);
+            k += curK;
+
+            Console.WriteLine($"{k} {current}");
+        }
     }
 
     static bool isEnd(ref string str)
