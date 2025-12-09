a48655f7fdc804b761da9b935c65e557d7b36dbb
unknown <max.xit234@gmail.com>
2025-12-09 15:17:00 +0300

    first part finish

--- day_3/Program.cs
+++ day_3/Program.cs
@@ -6,6 +6,43 @@ class Program
 {
     static void Main()
     {
+        string? inputLine;
+        int sum = 0;
+        List<string> arrayOfRows = new List<string>();
 
+        while (!string.IsNullOrEmpty(inputLine = Console.ReadLine()))
+        {
+            arrayOfRows.Add(inputLine);
+        }
+        foreach (var e in arrayOfRows)
+        {
+            sumJoltage(e, ref sum);
+        }
+        Console.WriteLine(sum);
+    }
+
+    static void sumJoltage(string str, ref int sum)
+    {
+        int maxOne = -1, maxTwo = -1;
+        int convertCharOne = 0, convertCharTwo = 0, indexOfMax = 0;
+        for (int i = 0; i < str.Length - 1; i++)
+        {
+            convertCharOne = int.Parse(str[i].ToString());
+            if (convertCharOne > maxOne)
+            {
+                maxOne = convertCharOne;
+                indexOfMax = i;
+            }
+        }
+        for (int j = indexOfMax + 1; j < str.Length; j++)
+        {
+            convertCharTwo = int.Parse(str[j].ToString());
+            if (convertCharTwo > maxTwo)
+            {
+                maxTwo = convertCharTwo;
+            }
+        }
+        Console.WriteLine($"maxone: {Convert.ToString(maxOne)} maxtwo {Convert.ToString(maxTwo)}");
+        sum += int.Parse(Convert.ToString(maxOne) + Convert.ToString(maxTwo));
     }
 }
