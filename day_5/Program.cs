235b6116efa6b44109aec9567530c7d98a9cc95e
unknown <max.xit234@gmail.com>
2025-12-17 23:36:00 +0300

    part_1

---
+++ day_5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string? inputLine;
+        double k = 0;
+        HashSet<double> set = new HashSet<double>();
+        List<double> ingridients = new List<double>();
+
+        while ((inputLine = Console.ReadLine()) != ' ')
+        {
+            addToSet(inputLine, ref set);
+        }
+        while (!string.IsNullOrEmpty(inputLine = Console.ReadLine()))
+        {
+            addToSet(inputLine, ref set);
+        }
+        Console.WriteLine(k);
+    }
+
+    static void addToSet(string str, ref HashSet<double> set)
+    {
+        double[] se = new double[str.Split("-")];
+        double start = se[0], end = se[1];
+        for (int i = start; i < end + 1; i++)
+        {
+            set.Add(i);
+        }
+    }
+
+    static void countFresh(string str, ref HashSet<double> set)
+    {
+        double[] se = new double[str.Split("-")];
+        double start = se[0], end = se[1];
+        for (int i = start; i < end + 1; i++)
+        {
+            set.Add(i);
+        }
+    }
+}
