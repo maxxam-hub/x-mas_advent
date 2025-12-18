08e5b905694e18a06ec492d9799786488362fcb2
unknown <max.xit234@gmail.com>
2025-12-18 19:14:57 +0300

    finish part_1

--- day_5/Program.cs
+++ day_5/Program.cs
@@ -7,32 +7,39 @@ class Program
     static void Main()
     {
         string? inputLine;
-        double k = 0;
-        HashSet<double> set = new HashSet<double>();
+        double k = 0, size = 0;
+        List<(double, double)> listOfTuples = new List<(double, double)>();
 
         while (!string.IsNullOrEmpty(inputLine = Console.ReadLine()))
         {
-            addToSet(inputLine, ref set);
+            addToList(inputLine, ref listOfTuples, ref size);
         }
         while (!string.IsNullOrEmpty(inputLine = Console.ReadLine()))
         {
-            countFresh(inputLine, ref set, ref k);
+            countFresh(inputLine, listOfTuples, ref k, size);
         }
         Console.WriteLine(k);
     }
 
-    static void addToSet(string str, ref HashSet<double> set)
+    static void addToList(string str, ref List<(double, double)> list, ref double size)
     {
         string[] se = str.Split("-");
         double start = Convert.ToInt64(se[0]), end = Convert.ToInt64(se[1]);
-        for (double i = start; i < end + 1; i++)
-        {
-            set.Add(i);
-        }
+        list.Add((start, end));
+        size++;
     }
 
-    static void countFresh(string str, ref HashSet<double> set, ref double k)
+    static void countFresh(string str, List<(double, double)> list, ref double k, double size)
     {
-        if (set.Contains(Convert.ToInt64(str))) k++;
+        double n = Convert.ToInt64(str);
+        for (int i = 0; i < size; i++)
+        {
+            if (n <= list[i].Item2 && n >= list[i].Item1)
+            {
+                Console.WriteLine($"I1 {list[i].Item1} I2 {list[i].Item2} n {n}");
+                k++;
+                break;
+            }
+        }
     }
 }
